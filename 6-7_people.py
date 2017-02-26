8b1645e1ce7ebdc6793a5950e7c66a441bcd9e0e
chriskirkpatrick <chris.s.kirkpatrick@gmail.com>
2017-02-27 00:42:43 +0100

    echo Mon Feb 27 00:42:43 CET 2017

--- 6-7_people.py
+++ 6-7_people.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
-person = {
-    'first_name': 'Anais',
-    'last_name': 'van Delft',
-    'age': '35',
-    'city': 'Kleve',
-    }
-#print(person)
-for item in person.values():
-    print(item)
+people = {
+	'anais' : {
+	  	'first_name': 'Anais',
+  		'last_name': 'van Delft',
+  		'age': '35',
+		'city': 'Kleve',
+		},
+	'chris' : {
+		'first_name': 'Chris',
+		'last_name': 'Kirkpatrick',
+		'age': '34',
+		'city': 'Kleve',
+		},
+	'will' : {
+		'first_name': 'William',
+		'last_name': 'Nylander',
+		'age': '20',
+		'city': 'Toronto',
+		},
+	}
+
+for people, info in people.items():
+    first_name = info['first_name']
+    last_name = info['last_name']
+    age = info['age']
+    city = info['city']
+    print("\nFirst name: " + first_name + "\nLast name: " + last_name + "\nAge: " + age + "\nCity: " + city)
