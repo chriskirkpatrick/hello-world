9d3c139f14f4080524971f26b033b981f3360202
chriskirkpatrick <chris.s.kirkpatrick@gmail.com>
2017-02-26 23:24:01 +0100

    just another commit yo

---
+++ 6-7_people.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+person = {
+    'first_name': 'Anais',
+    'last_name': 'van Delft',
+    'age': '35',
+    'city': 'Kleve',
+    }
+#print(person)
+for item in person.values():
+    print(item)
